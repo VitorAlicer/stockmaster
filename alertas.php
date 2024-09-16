c86b356ea15fb90dc223101fbd7f4849d58c5226
vitor <vitoralicer@gmail.com>
2024-09-16 13:32:47 -0300

    add alertas e avisos

---
+++ alertas.php
@@ -0,0 +1,20 @@
+
+<div id="alert-message-danger" class="alert alert-danger" style="display:none;">
+   <h4><i class="icon fa fa-ban"></i> Erro!</h4>
+   <span class="message"></span>
+</div>
+
+<div id="alert-message-success" class="alert alert-success" style="display:none;">
+   <h4><i class="icon fa fa-check"></i> Sucesso!</h4>
+   <span class="message"></span>
+</div>
+
+<div id="alert-message-info" class="alert alert-info" style="display:none;">
+   <h4><i class="icon fa fa-info"></i> Info!</h4>
+   <span class="message"></span>
+</div>
+
+<div id="alert-message-warning" class="alert alert-warning" style="display:none;">
+   <h4><i class="icon fa fa-warning"></i> Alerta!</h4>
+   <span class="message"></span>
+</div>
---
+++ avisos.php
@@ -0,0 +1,18 @@
+
+<div id="box-info" class="row" style="display:none;">
+   <div class="col-sm-9 col-sm-offset-1">
+      <div class="alert alert-info">
+         <h4><i class="icon fa fa-info"></i> Aviso:</h4>
+         <ul class="x-infos"></ul>
+      </div>
+   </div>
+</div>
+
+<div id="box-error" class="row" style="display:none;">
+   <div class="col-sm-9 col-sm-offset-1">
+      <div class="alert alert-danger">
+         <h4><i class="icon fa fa-ban"></i> Foram encontrados os seguintes erros:</h4>
+         <ul class="x-errors"></ul>
+      </div>
+   </div>
+</div>
