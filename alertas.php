42d211d5883f4d28a6ac7a495cdc1a44433b429d
ximitt <leoooooq@gmail.com>
2024-09-23 14:15:00 -0300

    Documentação alertas.php

--- alertas.php
+++ alertas.php
@@ -1,20 +1,23 @@
-
+<!-- Div para mensagens de erro, inicialmente oculta -->
 <div id="alert-message-danger" class="alert alert-danger" style="display:none;">
-   <h4><i class="icon fa fa-ban"></i> Erro!</h4>
-   <span class="message"></span>
+   <h4><i class="icon fa fa-ban"></i> Erro!</h4> <!-- Título da mensagem de erro com ícone -->
+   <span class="message"></span> <!-- Espaço para o texto da mensagem de erro -->
 </div>
 
+<!-- Div para mensagens de sucesso, inicialmente oculta -->
 <div id="alert-message-success" class="alert alert-success" style="display:none;">
-   <h4><i class="icon fa fa-check"></i> Sucesso!</h4>
-   <span class="message"></span>
+   <h4><i class="icon fa fa-check"></i> Sucesso!</h4> <!-- Título da mensagem de sucesso com ícone -->
+   <span class="message"></span> <!-- Espaço para o texto da mensagem de sucesso -->
 </div>
 
+<!-- Div para mensagens informativas, inicialmente oculta -->
 <div id="alert-message-info" class="alert alert-info" style="display:none;">
-   <h4><i class="icon fa fa-info"></i> Info!</h4>
-   <span class="message"></span>
+   <h4><i class="icon fa fa-info"></i> Info!</h4> <!-- Título da mensagem informativa com ícone -->
+   <span class="message"></span> <!-- Espaço para o texto da mensagem informativa -->
 </div>
 
+<!-- Div para mensagens de alerta, inicialmente oculta -->
 <div id="alert-message-warning" class="alert alert-warning" style="display:none;">
-   <h4><i class="icon fa fa-warning"></i> Alerta!</h4>
-   <span class="message"></span>
+   <h4><i class="icon fa fa-warning"></i> Alerta!</h4> <!-- Título da mensagem de alerta com ícone -->
+   <span class="message"></span> <!-- Espaço para o texto da mensagem de alerta -->
 </div>
