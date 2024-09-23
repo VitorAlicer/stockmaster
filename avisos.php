e0800849f1a69066dd49a348f7f40776d4773c7b
ximitt <leoooooq@gmail.com>
2024-09-23 14:17:00 -0300

    Documentação avisos.php

--- avisos.php
+++ avisos.php
@@ -1,18 +1,29 @@
-
+<!-- Início da seção para avisos informativos -->
 <div id="box-info" class="row" style="display:none;">
+   <!-- Coluna que ocupa 9/12 do espaço, centralizada com offset -->
    <div class="col-sm-9 col-sm-offset-1">
+      <!-- Caixa de alerta informativa -->
       <div class="alert alert-info">
+         <!-- Título do aviso com um ícone -->
          <h4><i class="icon fa fa-info"></i> Aviso:</h4>
+         <!-- Lista não ordenada para informações adicionais -->
          <ul class="x-infos"></ul>
       </div>
    </div>
 </div>
+<!-- Fim da seção para avisos informativos -->
 
+<!-- Início da seção para mensagens de erro -->
 <div id="box-error" class="row" style="display:none;">
+   <!-- Coluna que ocupa 9/12 do espaço, centralizada com offset -->
    <div class="col-sm-9 col-sm-offset-1">
+      <!-- Caixa de alerta de erro -->
       <div class="alert alert-danger">
+         <!-- Título do alerta de erro com um ícone -->
          <h4><i class="icon fa fa-ban"></i> Foram encontrados os seguintes erros:</h4>
+         <!-- Lista não ordenada para mostrar os erros encontrados -->
          <ul class="x-errors"></ul>
       </div>
    </div>
 </div>
+<!-- Fim da seção para mensagens de erro -->
